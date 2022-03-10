58d0f99209f59be28470f53c6199e10d4ca3dc7a
vildan-b <vildanb@gmx.de>
2022-03-10 10:30:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Filtering Code in Vue.js
+
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Filtering Code in Vue.js</h3>
+        <p> {{ message | toUpperCase }} </p>
+        <hr> <br>
+        <input type="text" v-model="searchText">
+        <hr> <br>
+        <ul>
+        <li v-for="city in filtered"> {{ city }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+  return {
+    message : "Filtering Code in Vue.js",
+    searchText : "",
+    cities : ["Berlin", "Hamburg","Munich","Frankfurt","Bremen","Düsseldorf",
+    "Stuttgart","Leipzig","Dortmund","Essen","Hannover","Nuremberg","Duisburg","Kassel"]
+  }
+},
+filters:{
+  toUpperCase(value){
+return value.toUpperCase();
+  }
+},
+computed:{
+  filtered(){
+    return this.cities.filter((element) =>{
+      return element.match(this.searchText);
+    })
+  }
+
+}
+
+}
+</script>
+
+<style>
+
+</style>
